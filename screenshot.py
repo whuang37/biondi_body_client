f95239a83d96e4ae3cbb1b2d81fb7e98d7b17fc9
unknown <whuang7337@gmail.com>
2020-07-22 19:55:18 -0700

    Fixed syntax in screenshots

--- screenshot.py
+++ screenshot.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 import pyautogui
 from PIL import ImageTk
 import datetime
@@ -19,52 +19,53 @@ class Application():
         root.attributes("-transparent", "blue")
         root.geometry('400x50+200+200')  # set new geometry
         root.title('Lil Snippy')
-        self.menu_frame = Frame(master, bg="blue")
-        self.menu_frame.pack(fill=BOTH, expand=YES)
+        self.menu_frame = tk.Frame(master, bg="blue")
+        self.menu_frame.pack(fill=tk.BOTH, expand=tk.YES)
 
-        self.buttonBar = Frame(self.menu_frame,bg="")
-        self.buttonBar.pack(fill=BOTH,expand=YES)
+        self.button_bar = tk.Frame(self.menu_frame,bg="")
+        self.button_bar.pack(fill=tk.BOTH,expand=tk.YES)
 
-        self.snipButton = Button(self.buttonBar, width=3, command=self.createScreenCanvas, background="green")
-        self.snipButton.pack(expand=YES)
+        self.snip_button = tk.Button(self.button_bar, width=3, command=self.create_screen_canvas, background="green")
+        self.snip_button.pack(expand=tk.YES)
         
-        self.screenshot = Toplevel(root)
+        self.screenshot = tk.Toplevel(root)
         self.screenshot.withdraw()
-        self.screenshot_frame = Frame(self.screenshot)
+        self.screenshot_frame = tk.Frame(self.screenshot)
         self.screenshot_frame.pack(expand=True)
 
-        self.master_screen = Toplevel(root)
+        self.master_screen = tk.Toplevel(root)
         self.master_screen.withdraw()
         self.master_screen.attributes("-transparent", "blue")
-        self.picture_frame = Frame(self.master_screen, background = "blue")
-        self.picture_frame.pack(fill=BOTH, expand=YES)
+        self.picture_frame = tk.Frame(self.master_screen, background = "blue")
+        self.picture_frame.pack(fill=tk.BOTH, expand=tk.YES)
 
-    def createScreenshotCanvas(self, img):
+    def create_screenshot_canvas(self, img):
         self.screenshot.deiconify()
         root.withdraw()
 
-        self.canvas1 = Canvas(self.screenshot_frame, width=img.width(), height = img.height(),borderwidth = 0, highlightthickness = 0 )
-        self.canvas1.pack(expand=YES)
-        self.canvas1.create_image(0, 0, image = img, anchor = NW)
+        self.canvas1 = tk.Canvas(self.screenshot_frame, width=img.width(), height = img.height(),borderwidth = 0, highlightthickness = 0 )
+        self.canvas1.pack(expand=tk.YES)
+        self.canvas1.create_image(0, 0, image = img, anchor = tk.NW)
         self.canvas1.img = img
-    def takeBoundedScreenShot(self, x1, y1, x2, y2):
+        
+    def take_bounded_screenshot(self, x1, y1, x2, y2):
         im = pyautogui.screenshot(region=(x1, y1, x2, y2))
         img = ImageTk.PhotoImage(im)
         x = datetime.datetime.now()
-        self.createScreenshotCanvas(img)
+        self.create_screenshot_canvas(img)
         # fileName = x.strftime("%f")
         # im.save(fileName + ".png")
 
-    def createScreenCanvas(self):
+    def create_screen_canvas(self):
         self.master_screen.deiconify()
         root.withdraw()
 
-        self.screenCanvas = Canvas(self.picture_frame, cursor="cross", bg="grey11")
-        self.screenCanvas.pack(fill=BOTH, expand=YES)
+        self.screen_canvas = tk.Canvas(self.picture_frame, cursor="cross", bg="grey11")
+        self.screen_canvas.pack(fill=tk.BOTH, expand=tk.YES)
 
-        self.screenCanvas.bind("<ButtonPress-1>", self.on_button_press)
-        self.screenCanvas.bind("<B1-Motion>", self.on_move_press)
-        self.screenCanvas.bind("<ButtonRelease-1>", self.on_button_release)
+        self.screen_canvas.bind("<ButtonPress-1>", self.on_button_press)
+        self.screen_canvas.bind("<B1-Motion>", self.on_move_press)
+        self.screen_canvas.bind("<ButtonRelease-1>", self.on_button_release)
 
         self.master_screen.attributes('-fullscreen', True)
         self.master_screen.attributes('-alpha', .3)
@@ -72,30 +73,30 @@ class Application():
         self.master_screen.attributes("-topmost", True)
 
     def on_button_release(self, event):
-        self.recPosition()
+        self.rec_position()
 
         if self.start_x <= self.curX and self.start_y <= self.curY:
             print("right down")
-            self.takeBoundedScreenShot(self.start_x, self.start_y, self.curX - self.start_x, self.curY - self.start_y)
+            self.take_bounded_screenshot(self.start_x, self.start_y, self.curX - self.start_x, self.curY - self.start_y)
 
         elif self.start_x >= self.curX and self.start_y <= self.curY:
             print("left down")
-            self.takeBoundedScreenShot(self.curX, self.start_y, self.start_x - self.curX, self.curY - self.start_y)
+            self.take_bounded_screenshot(self.curX, self.start_y, self.start_x - self.curX, self.curY - self.start_y)
 
         elif self.start_x <= self.curX and self.start_y >= self.curY:
             print("right up")
-            self.takeBoundedScreenShot(self.start_x, self.curY, self.curX - self.start_x, self.start_y - self.curY)
+            self.take_bounded_screenshot(self.start_x, self.curY, self.curX - self.start_x, self.start_y - self.curY)
 
         elif self.start_x >= self.curX and self.start_y >= self.curY:
             print("left up")
-            self.takeBoundedScreenShot(self.curX, self.curY, self.start_x - self.curX, self.start_y - self.curY)
+            self.take_bounded_screenshot(self.curX, self.curY, self.start_x - self.curX, self.start_y - self.curY)
 
-        self.exitScreenshotMode()
+        self.exit_screenshot_mode()
         return event
 
-    def exitScreenshotMode(self):
+    def exit_screenshot_mode(self):
         print("Screenshot mode exited")
-        self.screenCanvas.destroy()
+        self.screen_canvas.destroy()
         self.master_screen.withdraw()
         root.deiconify()
 
@@ -105,23 +106,23 @@ class Application():
 
     def on_button_press(self, event):
         # save mouse drag start position
-        self.start_x = self.screenCanvas.canvasx(event.x)
-        self.start_y = self.screenCanvas.canvasy(event.y)
+        self.start_x = self.screen_canvas.canvasx(event.x)
+        self.start_y = self.screen_canvas.canvasy(event.y)
 
-        self.rect = self.screenCanvas.create_rectangle(self.x, self.y, 1, 1, outline='red', width=3, fill="blue")
+        self.rect = self.screen_canvas.create_rectangle(self.x, self.y, 1, 1, outline='red', width=3, fill="blue")
 
     def on_move_press(self, event):
         self.curX, self.curY = (event.x, event.y)
         # expand rectangle as you drag the mouse
-        self.screenCanvas.coords(self.rect, self.start_x, self.start_y, self.curX, self.curY)
+        self.screen_canvas.coords(self.rect, self.start_x, self.start_y, self.curX, self.curY)
 
-    def recPosition(self):
+    def rec_position(self):
         print(self.start_x)
         print(self.start_y)
         print(self.curX)
         print(self.curY)
 
 if __name__ == '__main__':
-    root = Tk()
+    root = tk.Tk()
     app = Application(root)
-    root.mainloop()
+    root.mainloop() 
