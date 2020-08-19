b68cd6218522023be0254f1b5d2ee2bf459a069d
William Huang <whuang7337@gmail.com>
2020-08-19 16:27:55 -0700

    moved toolbar into screenshoteditor class

--- screenshot.py
+++ screenshot.py
@@ -4,76 +4,94 @@ from PIL import ImageTk, Image, ImageDraw, ImageFont
 from tkinter.colorchooser import askcolor
 from file_management import FileManagement
 
-class Toolbar(): # creates the toolbar and its related functions
-    counter =  1
-    DEFAULT_COLOR = 'white'
-    def __init__(self, master, canvas, im, h , w, body_info, folder_path, marker_canvas, new):
-        self.new = new
-        self.master = master
-        self.height = h
-        self.width = w
-        self.text_annotation = body_info["grid_id"] + " " + str(body_info["x"]) + ", " + str(body_info["y"])
-        if self.width > self.height: # find the appropriate margin for the text
-            self.margin = .05 * self.height
-        else:
-            self.margin = .05 * self.width
-        
+class ScreenshotEditor(tk.Frame):
+    def __init__(self, body_info, folder_path, marker_canvas, im, new):
+        self.body_info = body_info
+        self.folder_path = folder_path
+        self.marker_canvas = marker_canvas
         self.im = im
+        self.img = ImageTk.PhotoImage(im)
+        self.width = self.img.width()
+        self.height = self.img.height()
+        self.new = new
+        
         self.annotation = Image.new("RGBA", (self.width, self.height))
         self.draw = ImageDraw.Draw(self.annotation) #initialize a pillow drawing that runs in the background for saving annotations
         
-        self.canvas = canvas
-        self.counter = 1
-        self.undone = [] 
+        self.screenshot = tk.Toplevel()
+        # self.screenshot.withdraw()
         
-        self.brush_button = tk.Button(master, text = "brush", command = self.use_brush)
-        self.color_button = tk.Button(master, text = 'color', command = self.choose_color)
-        self.undo_button = tk.Button(master, text = 'undo', command = self.undo)
-        self.redo_button = tk.Button(master, text = 'redo', command = self.redo)
-        self.text_button = tk.Button(master, text = "top-left", command = self.text)
-        self.save_button = tk.Button(master, text = 'save', command = self.save)
+        self.screenshot_frame = tk.Frame(self.screenshot)
+        self.screenshot_frame.grid(row = 1, column = 0)
+        
+        self.create_screenshot_canvas()
+
+        self.toolbar_frame = tk.Frame(self.screenshot)
+        self.toolbar_frame.grid(row=0, column = 0)
+        
+        self.create_toolbar()
+        
+        self.setup()
+
+        
+    def create_screenshot_canvas(self):
+        self.screenshot_canvas = tk.Canvas(self.screenshot_frame, width = self.width, height = self.height,
+                                    borderwidth = 0, highlightthickness = 0)
+        self.screenshot_canvas.pack(expand = True)
+        self.screenshot_canvas.create_image(0, 0, image = self.img, anchor = "nw")
+        self.screenshot_canvas.img = self.img
+        self.screenshot_canvas.focus_set()
+        
+    def create_toolbar(self):
+        self.brush_button = tk.Button(self.toolbar_frame, text = "brush", command = self.use_brush)
+        self.color_button = tk.Button(self.toolbar_frame, text = 'color', command = self.choose_color)
+        self.undo_button = tk.Button(self.toolbar_frame, text = 'undo', command = self.undo)
+        self.redo_button = tk.Button(self.toolbar_frame, text = 'redo', command = self.redo)
+        self.text_button = tk.Button(self.toolbar_frame, text = "top-left", command = self.text)
+        self.save_button = tk.Button(self.toolbar_frame, text = 'save', command = self.save)
+        
         self.brush_button.grid(row = 0, column = 0)
         self.color_button.grid(row = 0, column = 1)
         self.undo_button.grid(row = 0, column = 2)
         self.redo_button.grid(row = 0, column = 3)
         self.text_button.grid(row = 0 , column = 4)
         self.save_button.grid(row = 0 , column = 5)
-        self.setup()
+        self.text_annotation = self.body_info["grid_id"] + " " + str(self.body_info["x"]) + ", " + str(self.body_info["y"])
+        if self.width > self.height: # find the appropriate margin for the text
+            self.margin = .05 * self.height
+        else:
+            self.margin = .05 * self.width
         
-        self.body_info = body_info
-        self.folder_path = folder_path
-        self.canvas.create_text(self.margin, self.margin, text = self.text_annotation, 
+        self.screenshot_canvas.create_text(self.margin, self.margin, text = self.text_annotation, 
                                 font =("Calibri", 14), anchor = "nw", fill = 'white', tag ="text") # creates text location
-        
-        self.marker_canvas = marker_canvas
-        
     
     def setup(self):
         self.old_x = None
         self.old_y = None
         self.line_width = 5
-        self.color = self.DEFAULT_COLOR
+        self.color = 'white'
         self.active_button = self.brush_button
-        self.canvas.bind('<B1-Motion>', self.paint)
-        self.canvas.bind('<ButtonRelease-1>', self.reset)
-        self.canvas.bind('<Control-z>', self.undo_bind) # used for undoing potential lines drawn
-        
+        self.screenshot_canvas.bind('<B1-Motion>', self.paint)
+        self.screenshot_canvas.bind('<ButtonRelease-1>', self.reset)
+        self.screenshot_canvas.bind('<Control-z>', self.undo_bind) # used for undoing potential lines drawn
+        self.counter = 1
+        self.undone = []
+
     def use_brush(self):
         self.activate_button(self.brush_button)
         
     def choose_color(self):
-        self.color = askcolor(color = self.color, parent = self.master)[1]
+        self.color = askcolor(color = self.color, parent = self.toolbar_frame)[1]
         
     def activate_button(self, some_button): # keeps brush button sunken as its used
         self.active_button.config(relief = "raised")
         some_button.config(relief = "sunken")
         self.active_button = some_button
         
-
     def paint(self, event): # creates the paint lines
         self.line_width = 5
         if self.old_x and self.old_y:
-            self.canvas.create_line(self.old_x, self.old_y, event.x, event.y, 
+            self.screenshot_canvas.create_line(self.old_x, self.old_y, event.x, event.y, 
                                     width = self.line_width, fill = self.color, 
                                     capstyle = "round", smooth = True, splinesteps = 36, 
                                     tag=['line' + str(self.counter)])
@@ -101,16 +119,16 @@ class Toolbar(): # creates the toolbar and its related functions
             self.text_button.configure(text='top-left')
         
     def text_position(self, x, y, position):
-        self.canvas.delete('text')
-        self.canvas.create_text(x, y, text = self.text_annotation, 
+        self.screenshot_canvas.delete('text')
+        self.screenshot_canvas.create_text(x, y, text = self.text_annotation, 
                                 font =("Calibri", 14), anchor = position, 
                                 fill = 'white', tag ="text")
     def undo(self): 
         self.counter -= 1
         currentundone = []
-        for item in self.canvas.find_withtag('line' + str(self.counter)):
-            currentundone.append(self.canvas.coords(item)) 
-        self.canvas.delete('line'+str(self.counter)) # deletes related line
+        for item in self.screenshot_canvas.find_withtag('line' + str(self.counter)):
+            currentundone.append(self.screenshot_canvas.coords(item)) 
+        self.screenshot_canvas.delete('line'+str(self.counter)) # deletes related line
         self.undone.append(currentundone) # appends list of undone for potential redoing
         
     def undo_bind(self, event): # enables ctrl-z bind to work
@@ -120,7 +138,7 @@ class Toolbar(): # creates the toolbar and its related functions
         try:
             currentundone = self.undone.pop() 
             for coords in currentundone: #pulls from list of undoes to recreate the first one
-                self.canvas.create_line(coords, width = self.line_width, fill = self.color, 
+                self.screenshot_canvas.create_line(coords, width = self.line_width, fill = self.color, 
                                     capstyle = "round", smooth = "TRUE", splinesteps = 36, tag=['line' + str(self.counter)])
             self.counter += 1
         except IndexError:
@@ -128,7 +146,7 @@ class Toolbar(): # creates the toolbar and its related functions
         
     def save(self):
         self.font = ImageFont.truetype("calibrib.ttf", 20) 
-        bounds = self.canvas.bbox("text")
+        bounds = self.screenshot_canvas.bbox("text")
         self.draw.text((bounds[0], bounds[1]), fill = 'white', 
                         font = self.font, anchor = "ne", text = self.text_annotation) #takes the bottom left coordinate of text and places the text on the pillow drawing
         
@@ -139,39 +157,6 @@ class Toolbar(): # creates the toolbar and its related functions
             GridMark(self.marker_canvas, self.folder_path, self.body_info)
         else: 
             self.annotation.save(self.folder_path + self.body_info["annotation_file_name"])
-
-class ScreenshotEditor(tk.Frame):
-    def __init__(self, body_info, folder_path, marker_canvas, new):
-        self.screenshot = tk.Toplevel()
-        self.screenshot.withdraw()
-
-        self.toolbar_frame = tk.Frame(self.screenshot)
-        self.toolbar_frame.grid(row=0, column = 0)
-        
-        self.screenshot_frame = tk.Frame(self.screenshot)
-        self.screenshot_frame.grid(row = 1, column = 0)
-        
-        self.body_info = body_info
-        self.folder_path = folder_path
-        self.new = new
-        self.marker_canvas = marker_canvas
-
-    def create_screenshot_canvas(self, im):
-        
-        self.screenshot.deiconify()
-        img = ImageTk.PhotoImage(im)
-        width = img.width()
-        height = img.height()
-        
-        self.screenshot_canvas = tk.Canvas(self.screenshot_frame, width = width, height = height,
-                                    borderwidth = 0, highlightthickness = 0)
-        self.screenshot_canvas.pack(expand=tk.YES)
-        self.screenshot_canvas.create_image(0, 0, image = img, anchor = "nw")
-        self.screenshot_canvas.img = img
-        self.my_toolbar = Toolbar(self.toolbar_frame, self.screenshot_canvas, im, height, width,
-                                    self.body_info, self.folder_path, self.marker_canvas, self.new)
-        self.screenshot_canvas.focus_set()
-
 
 class LilSnippy(tk.Frame):
     def __init__(self, master, body_info, folder_path, marker_canvas, *args, **kwargs):
@@ -196,8 +181,8 @@ class LilSnippy(tk.Frame):
     def take_bounded_screenshot(self, x1, y1, x2, y2):
         im = pyautogui.screenshot(region=(x1, y1, x2, y2))
         
-        self.screenshot_editor = ScreenshotEditor(self.body_info, self.folder_path, self.marker_canvas, True)
-        self.screenshot_editor.create_screenshot_canvas(im)
+        ScreenshotEditor(self.body_info, self.folder_path, self.marker_canvas, im, True)
+
 
     def create_screen_canvas(self):
         self.master_screen.deiconify()
